45867ffc2803acf3a8a7078844bb878225989493
CosmoCarpincho <cosmocarpincho@gmail.com>
2025-09-17 14:10:06 -0300

    feat: create software search (draft)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,18 +2,28 @@ import type { FC } from 'react'
 import './App.css'
 import Home from "./pages/Home"
 import { Link, Route, Routes } from 'react-router-dom'
+import { SoftwareSearchPage } from './pages/SoftwareSearchPage'
 
 
 
 const App: FC = () => {
   return (
     <div>
-      <nav>
+      <nav
+        style={{
+          display: "flex",
+          gap: "1rem",
+          padding: "1rem",
+          borderBottom: "1px solid #ccc",
+          marginBottom: "1rem"
+        }}>
         <Link to="/">Inicio</Link>
+        <Link to="/software-search">Software Search</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/software-search" element={<SoftwareSearchPage />} /> 
       </Routes>
     </div>
   )
---
+++ frontend/src/pages/SoftwareSearchPage.tsx
@@ -0,0 +1,6 @@
+import type { FC } from "react"
+import { SoftwareSearch } from "../components/SoftwareSearch"
+
+export const SoftwareSearchPage: FC = () => {
+  return <SoftwareSearch />
+}
