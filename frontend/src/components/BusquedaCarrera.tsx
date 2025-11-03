d2b85cd32a2e094a908f7e95a222c1e08a9fb007
CosmoCarpincho <cosmocarpincho@gmail.com>
2025-11-03 16:07:26 +0000

    primeras configuraciones y fix menores

--- frontend/src/components/BusquedaCarrera.tsx
+++ frontend/src/components/BusquedaCarrera.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useBusqueda } from  "../context/BusquedaContext";
 import { getApiBase } from "../lib/env";
 
