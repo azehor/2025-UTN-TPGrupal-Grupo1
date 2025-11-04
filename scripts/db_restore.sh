ae6ab84e79a1b58fd9cae2378ebf31c2b7ea6fc4
CosmoCarpincho <comsocarpincho@gmail.com>
2025-11-04 16:00:47 +0000

    infra: configuraciones de desarrollo y producción. Docker y docker-compose. Script para cargar db postgress cuando se pasa por backup custom.

---
+++ scripts/db_restore.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+FILE="./db_data/bd_nahue_v2.sql"
+CONTAINER="2025-utn-tpgrupal-grupo1-db-1"
+DBUSER="postgres"
+DBNAME="db_nahue"
+
+docker exec -i $CONTAINER pg_restore \
+  -U $DBUSER \
+  -d $DBNAME \
+  --clean --if-exists --no-owner --no-privileges < "$FILE"
+
+echo "✅ Restore completado"
